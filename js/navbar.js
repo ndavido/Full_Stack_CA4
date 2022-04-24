c70b61fd2f2a62addf408e075f59c8e56c99a145
ndavido <D00239107@student.dkit.ie>
2022-04-24 18:16:47 +0100

    Changes to direction

--- js/navbar.js
+++ js/navbar.js
@@ -11,6 +11,14 @@ function openNav() {
     document.getElementById("myNav").style.height = "100%";
 }
 
+function openNav2() {
+    document.getElementById("myNav2").style.height = "100%";
+}
+
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
+}
+
+function closeNav2() {
+    document.getElementById("myNav2").style.height = "0%";
 }
